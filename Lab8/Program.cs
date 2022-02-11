e38022a3e8f0d2a92a2111138ea80741736d9fef
Jon <JJKing619@gmail.com>
2022-02-11 12:54:37 -0500

    First Commit

---
+++ Lab8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+
+Console.WriteLine("Please enter a phrase you would like to reverse: ");
+string input = Console.ReadLine();
+ReverseStack(input);
+
+
+static string ReverseStack(string input)
+{
+
+    Stack<string> temp = new Stack<string>();
+    string[] words = input.Split(' ');
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        temp.Push(words[i]);
+    }
+
+    while (temp.Count != 0)
+    {
+        Console.Write(temp.Peek() + " ");
+        temp.Pop();
+    }
+    return input;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
